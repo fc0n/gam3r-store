1678251e7c7aeeee13e2539ea675423b64e6658c
Jean Gabriel <jeangabrieljb9@gmail.com>
2024-10-01 18:33:01 -0300

    feat: export interfaces

---
+++ frontend/src/app/core/index.ts
@@ -0,0 +1,2 @@
+export * from './parcelamento'
+export * from './produto'
---
+++ frontend/src/app/core/parcelamento/parcelamento.ts
@@ -0,0 +1,6 @@
+export default interface Parcelamento {
+    valorTotal: number
+    valorParcial: number
+    qtdeParcelas: number
+    taxaJuros: number
+}
