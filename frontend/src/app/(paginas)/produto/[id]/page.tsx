0f39b849cf30979ca807bac6479bef0fbce7976b
Jean Gabriel <jeangabrieljb9@gmail.com>
2024-10-02 19:04:41 -0300

    feat: adicionando pagina do produto selecionado

---
+++ frontend/src/app/(paginas)/produto/[id]/page.tsx
@@ -0,0 +1,21 @@
+import BannerCompra from "@/app/components/produto/bannerCompra"
+import InformacoesProduto from "@/app/components/produto/informacoesProduto"
+import ProdutoNaoEncontrado from "@/app/components/produto/produtoNaoEncontrado"
+import TituloProduto from "@/app/components/produto/tituloProduto"
+import { produtos } from "@/app/core"
+
+export default function PaginaProduto(props: any) {
+    const id = +props.params.id
+    const produto = produtos.find((produto) => produto.id === id)
+    return produto ? (
+        <div className="flex flex-col gap-20 container py-10">
+        <div className="flex flex-col gap-10">
+            <TituloProduto produto={produto} />
+            <InformacoesProduto produto={produto} />
+            <BannerCompra produto={produto} />
+        </div>
+    </div>
+    ) :  (
+        <ProdutoNaoEncontrado />
+    )
+}
