f83c8a82b6eb5df6b3692c0072d7d51fe6dc4ba2
Jean Gabriel <jeangabrieljb9@gmail.com>
2024-10-06 17:27:05 -0300

    feat: export interfaces pedido

---
+++ packages/core/src/pedido/index.ts
@@ -0,0 +1,9 @@
+import Pedido from "./pedido"
+import { FormaPagamento } from "./formaPagamento"
+import ItemPedido from "./itemPedido"
+import PedidoEntrega from "./pedidoEntrega"
+import { Status } from "./status"
+
+
+export { FormaPagamento, Status }
+export type { Pedido, ItemPedido, PedidoEntrega }
